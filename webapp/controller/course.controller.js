f86e637dd9c9f370844b4fb973b75e40d81b43a6
Radega1993 <rauldearriba@gmail.com>
2018-02-24 15:19:48 +0000

    menu end

---
+++ webapp/controller/course.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define(['jquery.sap.global','sap/ui/core/mvc/Controller','sap/ui/model/json/JSONModel'],
+	function(jQuery, Controller, JSONModel) {
+	"use strict";
+
+	var menuController = Controller.extend("coolapp1.coolapp1.controller.course", {
+	});
+
+
+	return menuController;
+
+});
--- webapp/controller/index.controller.js
+++ webapp/controller/index.controller.js
@@ -1,12 +1,18 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller","coolapp1/coolapp1/model/models"
+	"sap/ui/core/mvc/Controller", "coolapp1/coolapp1/model/models"
 ], function(Controller, models) {
 	"use strict";
 
 	return Controller.extend("coolapp1.coolapp1.controller.index", {
-		onInit: function(){
-			this.getView().setModel(models.createDataTableModel(),"EmployeeDataTable");
-			this.getView().setModel(models.createCourseTableModel(),"CourseDataTable");
+		onInit: function() {
+			this.getView().setModel(models.createDataTableModel(), "EmployeeDataTable");
+			this.getView().setModel(models.createCourseTableModel(), "CourseDataTable");
+		},
+		onMenuItem: function(event){
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo(event.getParameter("item").getKey());
 		}
+
 	});
+
 });
